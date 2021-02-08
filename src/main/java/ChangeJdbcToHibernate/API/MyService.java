bbd08cdd40b6f3fab7fefc6e27f47d6de5a772ee
Danila <kimoror@mail.ru>
2021-02-08 15:14:37 +0300

    Delete src directory

--- src/main/java/ChangeJdbcToHibernate/API/MyService.java
+++
@@ -1,4 +0,0 @@
-package ChangeJdbcToHibernate.API;
-
-public class MyService {
-}
--- src/main/java/ChangeJdbcToHibernate/MODEL/repository/OutcomesRepository.java
+++
@@ -1,9 +0,0 @@
-package ChangeJdbcToHibernate.MODEL.repository;
-
-import ChangeJdbcToHibernate.MODEL.entity.Outcomes;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OutcomesRepository extends JpaRepository<Outcomes, Long> {
-}
